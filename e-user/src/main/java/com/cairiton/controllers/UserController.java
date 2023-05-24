7b76068f46b264af6d68016303c6266ff1708f55
joaocairiton <joaocairiton@gmail.com>
2023-05-24 20:03:23 -0300

    metodo buscar por id configurado

--- e-user/src/main/java/com/cairiton/controllers/UserController.java
+++ e-user/src/main/java/com/cairiton/controllers/UserController.java
@@ -2,8 +2,16 @@ package com.cairiton.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cairiton.model.User;
@@ -22,6 +30,20 @@ public class UserController {
 	@GetMapping
 	public List<User> listar() {
 		return userRepository.findAll();
+	}
+	
+	@GetMapping("/{userId}")
+	public ResponseEntity<User> Buscar (@PathVariable Long userId){
+		return userRepository.findById(userId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public User adicionar(@Valid @RequestBody User user) {
+		return userRepository.save(user);
 	}
 	
 
--- e-user/src/main/java/com/cairiton/model/User.java
+++ e-user/src/main/java/com/cairiton/model/User.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -20,9 +23,19 @@ public class User {
 	@EqualsAndHashCode.Include
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
+	@Size(max = 60)
 	private String name;
 	private String password;
+	
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	private String email;
+	
+	@NotBlank
+	@Size(max = 20)
 	private String telefone;
 	
 
