f89e210d94670237f9ab7aa53bdcac923f320c9e
joaocairiton <joaocairiton@gmail.com>
2023-06-15 10:57:12 -0300

    entity product

---
+++ e-user/src/main/java/com/cairiton/model/Product.java
@@ -0,0 +1,79 @@
+package com.cairiton.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name_product;
+	
+	private String description;
+	
+	private String amount;
+	
+	private Double price;
+	
+	public Product() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName_product() {
+		return name_product;
+	}
+
+	public void setName_product(String name_product) {
+		this.name_product = name_product;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAmount() {
+		return amount;
+	}
+
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Product(Long id, String name_product, String description, String amount, Double price) {
+		super();
+		this.id = id;
+		this.name_product = name_product;
+		this.description = description;
+		this.amount = amount;
+		this.price = price;
+	}
+	
+	
+	
+
+}
---
+++ e-user/src/main/java/com/cairiton/model/Sale.java
@@ -0,0 +1,5 @@
+package com.cairiton.model;
+
+public class Sale {
+
+}
